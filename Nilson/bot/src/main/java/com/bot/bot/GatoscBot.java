68b7d5efc9f4dc2d546bbb0611f7c86471b2b020
nilson7 <mgtalsc@gmail.com>
2019-10-04 14:30:22 -0400

    Probando bot

---
+++ Nilson/bot/src/main/java/com/bot/bot/GatoscBot.java
@@ -0,0 +1,44 @@
+package com.bot.bot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+
+
+public class GatoscBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(final Update update) {
+        // Esta función se invocará cuando nuestro bot reciba un mensaje
+
+        // Se obtiene el mensaje escrito por el usuario
+        final String messageTextReceived = update.getMessage().getText();
+
+        // Se obtiene el id de chat del usuario
+        final long chatId = update.getMessage().getChatId();
+
+        // Se crea un objeto mensaje
+        SendMessage message = new SendMessage().setChatId(chatId).setText(messageTextReceived);
+
+        try {
+            // Se envía el mensaje
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // Se devuelve el nombre que dimos al bot al crearlo con el BotFather
+        return "EchoBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // Se devuelve el token que nos generó el BotFather de nuestro bot
+        return "999999:qwertyuiop";
+    }
+}
