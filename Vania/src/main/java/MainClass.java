06aa4f43ae77bc21e726dfe89951bfe567da4d68
LizbethVera <lizveraduran@gmail.com>
2019-10-05 21:09:20 -0400

    Agregando dependencias y programando el main y bot con la api de telegram

--- Vania/src/main/java/MainClass.java
+++ Vania/src/main/java/MainClass.java
@@ -1,2 +1,17 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 public class MainClass {
+    public static void main(String[] args){
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new inventariBot());
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- Vania/src/main/java/inventariBot.java
+++ Vania/src/main/java/inventariBot.java
@@ -1,2 +1,16 @@
-public class inventariBot {
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class inventariBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return "Liz bot";
+    }
+
+    public String getBotToken() {
+        return "954935122:AAF7m3tQBFO9ZHG0FG_h-iV55FJTM5WlHzQ";
+    }
 }
