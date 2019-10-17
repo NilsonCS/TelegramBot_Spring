8d15643ba8d59630f201c46018c5880e04be684c
LizbethVera <lizveraduran@gmail.com>
2019-10-17 11:44:05 -0400

    Imprimiendo los mensajes recibidos

--- Nilson/bot/src/main/java/com/bot/bot/handler/InventaryTelegramMessageHandle.java
+++ Nilson/bot/src/main/java/com/bot/bot/handler/InventaryTelegramMessageHandle.java
@@ -1,4 +1,24 @@
 package com.bot.bot.handler;
 
-public class InventaryTelegramMessageHandle {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class InventaryTelegramMessageHandle extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        System.out.println(update.getMessage().getFrom().getFirstName()+ ": " +update.getMessage().getText());
+        
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
 }
